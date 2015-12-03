df20b308cab6f6ccec63158d0d847a6d67ace69e
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2015-12-03 23:15:30 +0100

    Add Arbitrary Instance for AvlTree

--- test/Spec.hs
+++ test/Spec.hs
@@ -19,18 +19,23 @@ depthRight (Node _ _ _ r) = depth r
 recursiveDepth Leaf = -1
 recursiveDepth (Node _ _ suba subb) = 1 + max (recursiveDepth suba) (recursiveDepth subb)
 
+checkOrdering :: AvlTree Int -> Bool
 checkOrdering t = checkOrdering' t (const True)
   where checkOrdering' Leaf _ = True
         checkOrdering' (Node v _ suba subb) f = f v && checkOrdering' suba (<v) && checkOrdering' subb (>=v)
 
+checkDepth :: AvlTree Int -> Bool
 checkDepth Leaf = True
 checkDepth node@(Node _ d suba subb) = d == recursiveDepth node && checkDepth suba && checkDepth subb
 
-isLogarithmic :: AvlTree t -> Bool
+isLogarithmic :: AvlTree Int -> Bool
 isLogarithmic t = depth t <= floor (logBase 2 (fromIntegral (size t + 1)))
 
 canRotateLeft t = depthRight t > 0
 canRotateRight t = depthLeft t > 0
+
+instance (Arbitrary t, Ord t) => Arbitrary (AvlTree t) where
+  arbitrary = fromList <$> arbitrary
 
 main :: IO ()
 main = hspec $ do
@@ -48,10 +53,10 @@ main = hspec $ do
       property ((\l0 l1 -> let t = fromList l0 in all (\x -> (search t x) == (x `elem` l0)) l1) :: [Int] -> [Int] -> Bool)
 
     it "check that cached depth works as it should" $ do
-      property ((\l -> let t = fromList l in checkDepth t) :: [Int] -> Bool)
+      property checkDepth
 
     it "check ordering property" $ do
-      property ((\l -> let t = fromList l in checkOrdering t) :: [Int] -> Bool)
+      property checkOrdering
 
     it "check that depth is logarithm of the number of items" $ do
-      property ((\l -> let t = fromList l in isLogarithmic t) :: [Int] -> Bool)
+      property isLogarithmic
