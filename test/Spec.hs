69ac7a509546f7f672576ee2aeb00f83e3ba48f7
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2015-12-04 00:04:24 +0100

    Clean tests using (==>) and (===)

--- test/Spec.hs
+++ test/Spec.hs
@@ -19,44 +19,50 @@ depthRight (Node _ _ _ r) = depth r
 recursiveDepth Leaf = -1
 recursiveDepth (Node _ _ suba subb) = 1 + max (recursiveDepth suba) (recursiveDepth subb)
 
-checkOrdering :: AvlTree Int -> Bool
+checkOrdering :: Ord t => AvlTree t -> Bool
 checkOrdering t = checkOrdering' t (const True)
   where checkOrdering' Leaf _ = True
         checkOrdering' (Node v _ suba subb) f = f v && checkOrdering' suba (<v) && checkOrdering' subb (>=v)
 
-checkDepth :: AvlTree Int -> Bool
+checkDepth :: AvlTree t -> Bool
 checkDepth Leaf = True
 checkDepth node@(Node _ d suba subb) = d == recursiveDepth node && checkDepth suba && checkDepth subb
 
-isLogarithmic :: AvlTree Int -> Bool
+isLogarithmic :: AvlTree t -> Bool
 isLogarithmic t = depth t <= floor (logBase 2 (fromIntegral (size t + 1)))
 
 canRotateLeft t = depthRight t > 0
 canRotateRight t = depthLeft t > 0
 
-instance (Arbitrary t, Ord t) => Arbitrary (AvlTree t) where
-  arbitrary = fromList <$> arbitrary
+newtype IntList = IntList [Int] deriving (Show)
+newtype IntAvlTree = IntAvlTree (AvlTree Int) deriving (Show)
+
+instance Arbitrary IntAvlTree where
+  arbitrary = IntAvlTree . fromList <$> arbitrary
+
+instance Arbitrary IntList where
+  arbitrary = IntList <$> arbitrary
 
 main :: IO ()
 main = hspec $ do
   describe "tests" $ do
     it "check that insertion is ok and that it does not duplicate" $ do
-      property ((\l -> size (fromList l)  == length (nub l)) :: [Int] -> Bool)
+      property $ \(IntList l) -> size (fromList l) === length (nub l)
   
     it "check idempotence of toList -> fromList" $ do
-      property ((\l -> let t = fromList l in sort (nub l) == toList t) :: [Int] -> Bool)
+      property $ \(IntList l) -> let t = fromList l in sort (nub l) === toList t
 
     it "check that search works" $ do
-      property ((\l0 -> let t = fromList l0 in all (search t) l0) :: [Int] -> Bool)
+      property $ \(IntList l0) -> let t = fromList l0 in all (search t) l0
 
     it "heck that search does not find items that are not here"  $ do
-      property ((\l0 l1 -> let t = fromList l0 in all (\x -> (search t x) == (x `elem` l0)) l1) :: [Int] -> [Int] -> Bool)
+      property $ \(IntList l0) (IntList l1) -> let t = fromList l0 in all (\x -> (search t x) == (x `elem` l0)) l1
 
     it "check that cached depth works as it should" $ do
-      property checkDepth
+      property $ \(IntAvlTree t) -> checkDepth t
 
     it "check ordering property" $ do
-      property checkOrdering
+      property $ \(IntAvlTree t) -> checkOrdering t
 
     it "check that depth is logarithm of the number of items" $ do
-      property isLogarithmic
+      property $ \(IntAvlTree t) -> isLogarithmic t
