9424226e28956156bddbc04260f4239e1adebfed
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2015-12-04 10:17:13 +0100

    Add a few tests and organise them

--- test/Spec.hs
+++ test/Spec.hs
@@ -53,68 +53,86 @@ instance Arbitrary IntList where
 
 main :: IO ()
 main = hspec $ do
-  describe "tests" $ do
-    it "check that insertion is ok and that it does not duplicate" $ do
-      property $ \(IntList l) -> size (fromList l) === length (nub l)
+  describe "Public API" $ do
+    describe "singleton/empty + size/toList" $ do
+      it "create an empty" $ do
+        size empty `shouldBe` 0
+        (toList empty :: [Int]) `shouldBe` []
+
+      it "create a singleton" $ do
+        property $ \x -> size (singleton (x :: Int)) === 1
+
+      it "returns the right singleton" $ do
+        property $ \x -> toList (singleton (x :: Int)) === [x]
+
+    describe "fromList / toList / size" $ do
+      it "check that insertion is ok and that it does not duplicate" $ do
+        property $ \(IntList l) -> size (fromList l) === length (nub l)
   
-    it "check idempotence of toList -> fromList" $ do
-      property $ \(IntList l) -> let t = fromList l in sort (nub l) === toList t
+      it "check idempotence of toList -> fromList" $ do
+        property $ \(IntList l) -> let t = fromList l in sort (nub l) === toList t
 
-    it "check that search works" $ do
-      property $ \(IntList l0) -> let t = fromList l0 in all (search t) l0
+    describe "search" $ do
+      it "finds" $ do
+        property $ \(IntList l0) -> let t = fromList l0 in all (search t) l0
 
-    it "heck that search does not find items that are not here"  $ do
-      property $ \(IntList l0) (IntList l1) -> let t = fromList l0 in all (\x -> (search t x) == (x `elem` l0)) l1
+      it "does not find"  $ do
+        property $ \(IntList l0) (IntList l1) -> let t = fromList l0 in all (\x -> (search t x) == (x `elem` l0)) l1
 
-    it "check that cached depth works as it should" $ do
-      property $ \(IntAvlTree t) -> checkDepth t
+    describe "insertions" $ do
+      it "insert" $ do
+        property $ \(IntAvlTree t) (IntList l) -> let t' = insertMany t l in toList t' === sort (nub (toList t ++ l))
 
-    it "check ordering property" $ do
-      property $ \(IntAvlTree t) -> checkOrdering t
+      it "insertMany" $ do
+        property $ \(IntAvlTree t) x -> let t' = insert t (x :: Int) in toList t' === sort (nub (x:toList t))
 
-    it "check that depth is logarithm of the number of items" $ do
-      property $ \(IntAvlTree t) -> isLogarithmic t
+    describe "prettyPrint" $ do
+      it "contains the numbers" $ do
+        property $ \(IntAvlTree t) -> let l = map read (words (filter (/='.') (pprint t))) in sort l == toList t
 
-  describe "left rotation" $ do
-    it "keeps item" $ do
-      property $ \(IntAvlTree t) -> canRotateLeft t ==> toList t === toList (unsafeRotateLeft t)
-    it "keep ordering" $ do
-      property $ \(IntAvlTree t) -> canRotateLeft t ==> checkOrdering (unsafeRotateLeft t)
-    it "increses leftDepth" $ do
-      property $ \(IntAvlTree t) -> canRotateLeft t ==> depthLeft (unsafeRotateLeft t) > depthLeft t
-    it "decrease rightDepth" $ do
-      property $ \(IntAvlTree t) -> canRotateLeft t ==> depthRight (unsafeRotateLeft t) < depthRight t
+  describe "Private API / Properties" $ do
+    describe "fromList" $ do
+      it "cached depth are correct" $ do
+        property $ \(IntAvlTree t) -> checkDepth t
 
-  describe "right rotation" $ do
-    it "keeps item" $ do
-      property $ \(IntAvlTree t) -> canRotateRight t ==> toList t === toList (unsafeRotateRight t)
-    it "keep ordering" $ do
-      property $ \(IntAvlTree t) -> canRotateRight t ==> checkOrdering (unsafeRotateRight t)
-    it "increses rightDepth" $ do
-      property $ \(IntAvlTree t) -> canRotateRight t ==> depthRight (unsafeRotateRight t) > depthRight t
-    it "decrease leftDepth" $ do
-      property $ \(IntAvlTree t) -> canRotateRight t ==> depthLeft (unsafeRotateRight t) < depthLeft t
+      it "ordering property" $ do
+        property $ \(IntAvlTree t) -> checkOrdering t
 
-  describe "insert" $ do
-    it "insert" $ do
-      property $ \(IntAvlTree t) x -> let t' = insert t (x :: Int) in toList t' === sort (nub (x:toList t))
-    it "keep ordering" $ do
-      property $ \(IntAvlTree t) x -> checkOrdering (insert t (x :: Int))
-    it "keeps correct depth" $ do
-      property $ \(IntAvlTree t) x -> checkDepth (insert t (x :: Int))
-    it "keep log behavior" $ do
-      property $ \(IntAvlTree t) x -> matchTheAvlRule (insert t (x :: Int))
+      it "balance property" $ do
+        property $ \(IntAvlTree t) -> isLogarithmic t
 
-  describe "insertMany" $ do
-    it "insert" $ do
-      property $ \(IntAvlTree t) (IntList l) -> let t' = insertMany t l in toList t' === sort (nub (toList t ++ l))
-    it "keep ordering" $ do
-      property $ \(IntAvlTree t) (IntList l)  -> checkOrdering (insertMany t l)
-    it "keeps correct depth" $ do
-      property $ \(IntAvlTree t) (IntList l) -> checkDepth (insertMany t l)
-    it "keep log behavior" $ do
-      property $ \(IntAvlTree t) (IntList l) -> matchTheAvlRule (insertMany t l)
+    describe "insert" $ do
+      it "keep ordering" $ do
+        property $ \(IntAvlTree t) x -> checkOrdering (insert t (x :: Int))
+      it "keeps correct depth" $ do
+        property $ \(IntAvlTree t) x -> checkDepth (insert t (x :: Int))
+      it "keep log behavior" $ do
+        property $ \(IntAvlTree t) x -> matchTheAvlRule (insert t (x :: Int))
 
-  describe "prettyPrint" $ do
-    it "contains the numbers" $ do
-      property $ \(IntAvlTree t) -> let l = map read (words (filter (/='.') (pprint t))) in sort l == toList t
+    describe "insertMany" $ do
+      it "keep ordering" $ do
+        property $ \(IntAvlTree t) (IntList l)  -> checkOrdering (insertMany t l)
+      it "keeps correct depth" $ do
+        property $ \(IntAvlTree t) (IntList l) -> checkDepth (insertMany t l)
+      it "keep log behavior" $ do
+        property $ \(IntAvlTree t) (IntList l) -> matchTheAvlRule (insertMany t l)
+
+    describe "left rotation" $ do
+      it "keeps item" $ do
+        property $ \(IntAvlTree t) -> canRotateLeft t ==> toList t === toList (unsafeRotateLeft t)
+      it "keep ordering" $ do
+        property $ \(IntAvlTree t) -> canRotateLeft t ==> checkOrdering (unsafeRotateLeft t)
+      it "increses leftDepth" $ do
+        property $ \(IntAvlTree t) -> canRotateLeft t ==> depthLeft (unsafeRotateLeft t) > depthLeft t
+      it "decrease rightDepth" $ do
+        property $ \(IntAvlTree t) -> canRotateLeft t ==> depthRight (unsafeRotateLeft t) < depthRight t
+
+    describe "right rotation" $ do
+      it "keeps item" $ do
+        property $ \(IntAvlTree t) -> canRotateRight t ==> toList t === toList (unsafeRotateRight t)
+      it "keep ordering" $ do
+        property $ \(IntAvlTree t) -> canRotateRight t ==> checkOrdering (unsafeRotateRight t)
+      it "increses rightDepth" $ do
+        property $ \(IntAvlTree t) -> canRotateRight t ==> depthRight (unsafeRotateRight t) > depthRight t
+      it "decrease leftDepth" $ do
+        property $ \(IntAvlTree t) -> canRotateRight t ==> depthLeft (unsafeRotateRight t) < depthLeft t
